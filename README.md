39cae069531938df78d5f70ea02b2105df4b269c
Guilherme-Quintino-Silva <guilherme.luiz.quintino@gmail.com>
2024-06-19 22:56:28 -0300

    feat: Inserindo effect, set e update na pratica.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 <h1>Treino inicial para entendimento do Signals e demais pontos do Angular 17</h1>
-<p>Trabalhando com <strong>Signals</strong>, Update e Computed na prática.</p>
+<p>Trabalhando com <strong>Signals</strong>, Signal, Update, Set, Effect e Computed na prática.</p>
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +8,38 @@ import { Component, computed, signal } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  protected count = signal(0);
-  protected countDouble = computed(() => this.count() * 2)
-  constructor() {}
-  increment() {
+  protected count = signal<number>(0);
+  protected name = signal<string>("Guilherme");
+  protected countDouble = computed(() => this.count() * 2);
+  protected arrayNumber = signal([1, 2, 3]);
+  protected lastNumber: number = 5;
+  constructor() {
+    effect(() => {
+      if (this.count() > 10 && this.count() < 13) {
+        alert("Usando uma condicional no Effect do Signals.");
+      }
+    });
+  }
+  increment(): void {
     this.count.update(comp => comp + 1);
   }
-  decrement() {
+  decrement(): void {
     this.count.update(comp => comp - 1);
+  }
+  setName(): void {
+    this.name.set("Quintino");
+  }
+  updateName(): void {
+    this.name.update((oldName: string): string => {
+      console.log(oldName);
+      return "Silva";
+    });
+  }
+  updateChangeArray() {
+    this.lastNumber += 1;
+    this.arrayNumber.update(currentArray => {
+      return [...currentArray, this.lastNumber];
+    })
+    console.log(this.arrayNumber());
   }
 }
